c169a21e78e0f68c9d796ebe5078426b6ff33ecb
Mogreine <north7311@gmail.com>
2019-06-29 19:08:28 +0400

    Fixed paths

--- src/utils/video_cutter.py
+++ src/utils/video_cutter.py
@@ -1,18 +1,21 @@
 import cv2
 import os
 
+
 def cut(video_path, frame_count, pics_path, name):
     video_cap = cv2.VideoCapture(video_path)
-    frame_current = 0
     frame_number = 0
+    curr_frame = 0
     while video_cap.isOpened():
         success, frame = video_cap.read()
         if not success:
             break
-        if frame_current < frame_count:
-            frame_current += 1
+
+        if curr_frame < frame_count:
+            curr_frame += 1
             continue
-        frame_current = 0
+        curr_frame = 0
+
         cv2.imwrite(os.path.join(pics_path, name + str(frame_number) + '.jpg'), frame)
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -22,5 +25,6 @@ def cut(video_path, frame_count, pics_path, name):
     video_cap.release()
     cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
-    cut('test.mp4', 120, 'G:\work\ML\TrainDetection\pics', 'train_test')
+    cut('../../data/videos/test_video.mp4', 480, '../../data/images/tmp/', 'train_test')
