4fbd7d1c4f516f688c04ebe772eb55c995a10c9e
P4ll <cornelius.weling@gmail.com>
2019-07-05 20:47:29 +0400

    add shift in mean

--- src/train_count.py
+++ src/train_count.py
@@ -81,7 +81,7 @@ class TrainCounter(object):
         roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
         cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
         term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
-        #rect.lu_x += 200
+        rect.lu_x += 100
         while capture.isOpened():
             ret, frame = capture.read()
             if not ret:
