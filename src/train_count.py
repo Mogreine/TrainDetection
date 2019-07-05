b7c292dceaedc0bad9154bd516bfb98e1a3c8753
P4ll <cornelius.weling@gmail.com>
2019-07-05 20:07:55 +0400

    add train counter

--- src/train_count.py
+++ src/train_count.py
@@ -27,7 +27,6 @@ class Rectangle(object):
     def get_with_params(self):
         return self.lu_x, self.lu_y, self.width, self.height
 
-
 class TrainCounter(object):
     train_count = 0
     threshold = 5
@@ -35,28 +34,28 @@ class TrainCounter(object):
 
     def __init__(self, path_to_video):
         self.path_to_video = path_to_video
-
+    
     def camshift(self, rect: Rectangle):
         capture = cv2.VideoCapture(self.path_to_video)
         ret, frame = capture.read()
         track_window = rect.get_with_params()
         roi = frame[rect.lu_y:rect.lu_y + rect.height, rect.lu_x:rect.lu_x + rect.width]
-        hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv_roi, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
-        roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
-        cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
-        term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+        hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+        roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+        cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+        term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
         rect.lu_x += 200
         while capture.isOpened():
             ret, frame = capture.read()
             if not ret:
                 break
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+            dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
             ret, track_window = cv2.CamShift(dst, track_window, term_crit)
             pts = cv2.boxPoints(ret)
             pts = np.int0(pts)
-            mm = pts.min(axis=0)[0]
+            mm = pts.min(axis = 0)[0]
             if mm <= self.threshold:
                 self.train_count += 1
                 print(self.train_count)
