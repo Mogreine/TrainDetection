c8415290bbef3a2ae81a04edbda38f071e23ef0c
Mogreine <north7311@gmail.com>
2019-07-05 20:44:35 +0400

    Refactored

--- src/train_count.py
+++ src/train_count.py
@@ -41,22 +41,22 @@ class TrainCounter(object):
         ret, frame = capture.read()
         track_window = rect.get_with_params()
         roi = frame[rect.lu_y:rect.lu_y + rect.height, rect.lu_x:rect.lu_x + rect.width]
-        hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
-        roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-        cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
-        term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+        hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_roi, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
+        roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
+        cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+        term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
         rect.lu_x += 200
         while capture.isOpened():
             ret, frame = capture.read()
             if not ret:
                 break
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+            dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
             ret, track_window = cv2.CamShift(dst, track_window, term_crit)
             pts = cv2.boxPoints(ret)
             pts = np.int0(pts)
-            mm = pts.min(axis = 0)[0]
+            mm = pts.min(axis=0)[0]
             if mm <= self.threshold:
                 self.train_count += 1
                 print(self.train_count)
