5dad19c1888162de574aea4a7dfcbe793a5a14ab
P4ll <cornelius.weling@gmail.com>
2019-07-15 10:36:44 +0400

    add video capture in meanShift func

--- src/train_count.py
+++ src/train_count.py
@@ -30,8 +30,7 @@ class Rectangle(object):
 
 class TrainCounter(object):
     train_count = 0
-    threshold = 5
-    start_min = 100
+    threshold = 20
 
     def __init__(self, path_to_video):
         self.path_to_video = path_to_video
@@ -73,22 +72,30 @@ class TrainCounter(object):
         capture.release()
         cv2.destroyAllWindows()
 
-    def meanshift(self, rect: Rectangle):
+    def meanshift(self, rect: Rectangle, path_to_save = None):
         capture = cv2.VideoCapture(self.path_to_video)
+        if path_to_save is not None:
+            width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = int(capture.get(cv2.CAP_PROP_FPS))
+            vwriter = cv2.VideoWriter(path_to_save,
+                            cv2.VideoWriter_fourcc(*'MJPG'),
+                            fps, (width, height))
         ret, frame = capture.read()
         track_window = rect.get_with_params()
         roi = frame[rect.lu_y:rect.lu_y + rect.height, rect.lu_x:rect.lu_x + rect.width]
-        hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV) # COLOR_BGR2HSV
         mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
         roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-        cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+        cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX) # NORM_MINMAX
         term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
-        rect.lu_x += 75
+        rect.lu_x += 100
+        font = cv2.FONT_HERSHEY_SIMPLEX
         while capture.isOpened():
             ret, frame = capture.read()
             if not ret:
                 break
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # COLOR_BGR2HSV
             dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
             ret, track_window = cv2.meanShift(dst, track_window, term_crit)
             x, y, w, h = track_window
@@ -103,10 +110,15 @@ class TrainCounter(object):
                 roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
                 cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
             img2 = cv2.rectangle(frame, (x, y), (x + w, y + h), 255, 2)
+            if path_to_save is not None:
+                img2 = cv2.putText(img2, str(self.train_count), (10,500), font, 4,(255, 0, 0), 2, cv2.LINE_AA)
+                vwriter.write(img2)
             cv2.imshow('img2', img2)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         capture.release()
+        if path_to_save is not None:
+            vwriter.release()
         cv2.destroyAllWindows()
 
 
